78b421c89ab4dddfa40ed79323038dcb45ac9024
lethurh <gugulethupraxedis@gmail.com>
2024-12-18 09:22:57 +0000

    1. Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,7 +1,45 @@
-from flask import Blueprint, abort
+#!/usr/bin/env python3
+""" Module of Index views
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
 
-app_views = Blueprint('app_views', _name_)
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'])
-def get_unauthorized():
+@app_views.route('/status', strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - Aborts
+    """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/unauthorized
+    Return:
+      - Aborts
+    """
+    abort(403)
